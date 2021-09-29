bb80159cdb6261f09de5b41a78f50e3ba5597e2b
fredagbona <fredagbona@gmail.com>
2021-09-29 23:54:41 +0200

    users added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
+  get 'relationships/create'
+  get 'relationships/destroy'
+  resources :users, only: [:index]
+  resources :relationships, only: [:create, :destroy]
   get 'comments/create'
   resources :blogs do
     resources :comments
